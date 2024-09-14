31acc1251c90238389056537bdd7499ed3a38d73
Wendy Xiao <wendyxiao125@gmail.com>
2024-09-14 16:14:02 -0400

    audio recorder library

--- recordingreader.py
+++ recordingreader.py
@@ -1,10 +1,14 @@
 from audio_recorder import AudioToTextRecorder
 def main():
+    
     recorder = AudioToTextRecorder()
     recorder.start()
-    print(recorder.text())
+    while True:
+        print(recorder.text())
+        if recorder.text() == "Stop.":
+            break
     recorder.stop()
-    
+    recorder.shutdown()
 
 if __name__ == '__main__':
     main()
